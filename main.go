79aaab2a77e709140469c6d6f99d1ff1839bdefd
RerdRice <48865193+RerdRice@users.noreply.github.com>
2023-06-05 16:30:14 -0500

    Update main.go

--- main.go
+++ main.go
@@ -27,11 +27,36 @@ func init() {
 }
 
 func main() {
+  //This creates a new discord session with the token defined above
   dg, err := discordgo.New("Bot " + Token)
   if err != nil {
     fmt.Println("error creating Discord sesssion, ", err)
     return
   }
   
+   // Register the messageCreate func as a callback for MessageCreate events.
+   dg.AddHandler(messageCreate)  
+    
+   //recieve only message events
+   dg.Identify.Intents = discordgo.IntentsGuildMessages
+    
+    err = dg.Open()
+    if err != nil {
+        fmt.Println("error opening connection," err)
+        return
+    }
+    
+    //chill till terminated
+    fmt.Println("Bot is now running. Use CTRL-C to exit.")
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+    
+    //close the session
+    dg.Close()
+    
+    type Gopher struct {
+        Name string `json: "name"`
+    }
   //todo
   //handler, identify, error handling, running message, close dg session
